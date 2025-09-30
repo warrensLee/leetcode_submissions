8132cc5b183a715789a727d2abb13dece5361947
Warren Roberts <91992926+warrensLee@users.noreply.github.com>
2025-09-30 11:19:17 -0500

    [1] [Time Beats: 16.99%] [Memory Beats: 78.72%] - LeetPush

---
+++ Two-Sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = 0; j < nums.size(); j++)
+            {
+                if (i == j)
+                {
+                    break;
+                }
+                if (nums[i] + nums[j] == target)
+                {
+                    return {j,i};
+                }
+
+            }
+        }
+        return {};
+        
+    }
+};
+
+
+
+
+
